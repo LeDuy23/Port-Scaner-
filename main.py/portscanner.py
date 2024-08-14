67f403706328b65bc40931d8da3551def7f3756d
LeDuy23 <143040215+LeDuy23@users.noreply.github.com>
2024-08-13 18:23:05 -0700

    Works

--- main.py/portscanner.py
+++ main.py/portscanner.py
@@ -1,41 +1,63 @@
-import socket 
-import threading
+# Multithreaded Port Scanner
+# by NeuralNine Copyright (c) 2019
 from queue import Queue
+import socket
+import threading
+print("Please enter IP address: ")
+target = input()
+queue = Queue()
+open_ports = []
 
-print('Enter IP address: ')
-target = input() 
-queue = Queue() 
-open_port = [] 
 
-def portscan(port): 
+def portscan(port):
     try:
-     sock = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
-     sock.connect((target , port))
-     return True
-    except: 
-        return False 
-def fill_queue(port_list): 
-    for port in port_list: 
-        queue.put(port)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((target, port))
+        return True
+    except:
+        return False
 
-def worker(): 
-    while not queue.empty(): 
-        port = queue.get() 
+def get_ports(mode):
+    if mode == 1:
+        for port in range(1, 1024):
+            queue.put(port)
+    elif mode == 2:
+        for port in range(1, 49152):
+            queue.put(port)
+    elif mode == 3:
+        ports = [20, 21, 22, 23, 25, 53, 80, 110, 443]
+        for port in ports:
+            queue.put(port)
+    elif mode == 4:
+        ports = input("Enter your ports (seperate by blank):")
+        ports = ports.split()
+        ports = list(map(int, ports))
+        for port in ports:
+            queue.put(port)
+
+def worker():
+    while not queue.empty():
+        port = queue.get()
         if portscan(port):
-            print("Port{} is open!".format(port))
-            open_port.append(port)
-            
-port_list = range(1,1024)
-fill_queue(port_list) 
+            print("Port {} is open!".format(port))
+            open_ports.append(port)
 
-thread_list = []
+def run_scanner(threads, mode):
 
-for t in range(10): 
-    thread = threading.Thread(target = worker)
-    thread_list.append(thread)
+    get_ports(mode)
 
-for thread in thread_list: 
-    thread.join()
-    
-print("Open ports are: " , open_port)
-    
+    thread_list = []
+
+    for t in range(threads):
+        thread = threading.Thread(target=worker)
+        thread_list.append(thread)
+
+    for thread in thread_list:
+        thread.start()
+
+    for thread in thread_list:
+        thread.join()
+
+    print("Open ports are:", open_ports)
+
+run_scanner(100, 1)
